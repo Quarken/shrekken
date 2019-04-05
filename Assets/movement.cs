c8b7dbe0aee15829eebad0f21fb899ce444b157c
matiasfrank <matiasfrankj@gmail.com>
2019-04-05 23:45:55 +0200

    Changed to networkbehvaoir

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class movement : MonoBehaviour {
+public class movement : NetworkBehaviour {
 
 	public Rigidbody2D rb;
 	// Use this for initialization
@@ -12,6 +13,8 @@ public class movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (!isLocalPlayer) return;
+		
 		if (Input.GetKey(KeyCode.RightArrow)) {
 			rb.AddForce(new Vector2(15, 0));
 		}
