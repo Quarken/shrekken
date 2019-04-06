f47341668a6e11820c99f9314bf3b75e0e0eb948
Esben Bjerre <hej@esbenbjerre.dk>
2019-04-06 22:55:31 +0200

    djdj

--- Assets/ChooseCharacter.cs
+++ Assets/ChooseCharacter.cs
@@ -9,6 +9,9 @@ public class ChooseCharacter : MonoBehaviour{
     public Sprite shrek;
     public Sprite shreik;
     public Sprite shrump;
+
+    public string player1_name;
+    public string player2_name;
     
     private SpriteRenderer player1_sprite;
     private SpriteRenderer player2_sprite;
@@ -27,15 +30,19 @@ public class ChooseCharacter : MonoBehaviour{
     }
 
     public void playButton() {
+        PlayerInfo.player1 = player1_name;
+        PlayerInfo.player2 = player2_name;
         SceneManager.LoadScene("SampleScene");
     }
 
     public void selectShrek() {
         if (counter % 2 == 0) {
             player1_sprite.sprite = shrek;
+            player1_name = "Shrek";
         }
         else {
             player2_sprite.sprite = shrek;
+            player2_name = "Shrek";
         }
         counter++;
     }
@@ -44,9 +51,11 @@ public class ChooseCharacter : MonoBehaviour{
     public void selectShreik() {
         if (counter % 2 == 0) {
             player1_sprite.sprite = shreik;
+            player1_name = "Shreikh";
         }
         else {
             player2_sprite.sprite = shreik;
+            player2_name = "Shreikh";
         }
         counter++;
     }
@@ -54,9 +63,11 @@ public class ChooseCharacter : MonoBehaviour{
     public void selectShrump() {
         if (counter % 2 == 0) {
             player1_sprite.sprite = shrump;
+            player1_name = "Shrump";
         }
         else {
             player2_sprite.sprite = shrump;
+            player2_name = "Shrump";
         }
         counter++;
     }
---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerInfo {
+   
+    public static string player1;
+    public static string player2;
+
+}
