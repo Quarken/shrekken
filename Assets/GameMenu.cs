d88e58ef44aea2c4db733f4f26e7d870ff744a7c
Anders <au588409@uni.au.dk>
2019-04-06 15:45:56 +0200

    Menuer

---
+++ Assets/GameMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameMenu : MonoBehaviour{
+
+    public InputField ipInput;
+    public TextMeshProUGUI joinGameText;
+
+    public void Start() {
+
+    }
+
+    public void CreateGame() {
+
+    }
+
+    public void JoinGame() {
+        
+    }
+    
+    public void BackToMain() {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
