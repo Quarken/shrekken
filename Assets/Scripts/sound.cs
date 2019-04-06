f9d6b99be594e76888b4e8e83d79370abffb6b72
matiasfrank <matiasfrankj@gmail.com>
2019-04-06 21:12:22 +0200

    Its all ogre now

--- Assets/Scripts/sound.cs
+++ Assets/Scripts/sound.cs
@@ -6,6 +6,7 @@ public class sound : MonoBehaviour
 {
     public AudioSource startSound;
     public AudioSource backgroundSound;
+    public AudioSource itsOgreSound;
     public bool silent = false;    
     // Start is called before the first frame update
     void Start() {
@@ -20,5 +21,9 @@ public class sound : MonoBehaviour
         startSound.Play(0);   
         yield return new WaitForSeconds(startSound.clip.length);
         backgroundSound.Play();
+    }
+
+    public void PlayItsOgre() {
+        itsOgreSound.Play();
     }
 }
