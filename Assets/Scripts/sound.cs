5fe72bbc76b67ee07824db22389b359e0bb53211
matiasfrank <matiasfrankj@gmail.com>
2019-04-06 20:57:32 +0200

    Added wee bit o sound

---
+++ Assets/Scripts/sound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour
+{
+    public AudioSource startSound;
+    public AudioSource backgroundSound;
+    
+    // Start is called before the first frame update
+    void Start() {
+        backgroundSound.Play();
+        StartCoroutine(PlayStartSound());
+    }
+
+    IEnumerator PlayStartSound() {
+        yield return new WaitForSeconds(3);
+        backgroundSound.Pause();
+        startSound.Play(0);   
+        yield return new WaitForSeconds(startSound.clip.length);
+        backgroundSound.Play();
+    }
+}
