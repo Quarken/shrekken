9dfe9264ea36618921aba97063a9b33d5dc796e1
Ahmad Saleh <201709284@post.au.dk>
2019-04-06 02:18:36 +0200

    Movement state synchronization

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -6,16 +6,31 @@ using UnityEngine.Networking;
 public class movement : NetworkBehaviour {
 
 	public Rigidbody2D rb;
+
+	public int movement_speed = 5;
+
 	// Use this for initialization
 	void Start () {
-		
+	}
+
+	[Command]
+	void CmdMove(float axis_value) {
+		rb.AddForce(Vector2.right * axis_value * movement_speed);
+		RpcUpdatePosition(rb.transform.position);
+	}
+
+	[ClientRpc]
+	void RpcUpdatePosition(Vector2 position) {
+		rb.transform.position = position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (!isLocalPlayer) return;
-		if (Input.GetKey(KeyCode.RightArrow)) {
-			rb.AddForce(new Vector2(5, 0));
+		
+		var horizontal_value = Input.GetAxis("Horizontal");
+		if (horizontal_value != 0) {
+			CmdMove(horizontal_value);
 		}
 	}
 }
