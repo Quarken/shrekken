6b22290f4f01a66fe6b369994f01368740642e5b
matiasfrank <matiasfrankj@gmail.com>
2019-04-06 11:07:08 +0200

    The best git fix ever

--- Assets/Scripts/Shrek.cs
+++ Assets/Scripts/Shrek.cs
@@ -5,81 +5,63 @@ using UnityEngine;
 public class Shrek : MonoBehaviour
 {
     private static string ground = "Ground";
-    private bool isOnGround = true;
-    private bool hasHitGround = true;
-    private readonly int movSpeed = 30;
-    private readonly int jumpHeight = 40;
-
-    private readonly int gravity = 60;
+    private bool isOnGround = false;
+    private readonly int movSpeed = 15;
+    private readonly int jumpHeight = 10;
     private Rigidbody2D rb;
     private Animator animator;
     private SpriteRenderer sprite;
-    private Collider2D collider;
+    private Vector2 prevVel;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
-        collider = GetComponent<Collider2D>();
-    }
-
-    void FixedUpdate() {
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector2.down, 4);
-        isOnGround = hit.collider != null;
-
-        if(!isOnGround) {
-            rb.velocity += Vector2.down * gravity * Time.fixedDeltaTime;
-            hasHitGround = false;
-        }
-        else if(!hasHitGround) {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-            hasHitGround = true;
-        }
+        rb.drag = 0;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-            if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-                rb.velocity = new Vector2(-movSpeed, rb.velocity.y);
-                animator.SetBool("isWalking", true);
-                sprite.flipX = true;
-            }
-            if (Input.GetKeyDown(KeyCode.RightArrow)) {
-                rb.velocity = new Vector2(movSpeed, rb.velocity.y);
-                animator.SetBool("isWalking", true);
-                sprite.flipX = false;
-            }
-            if (Input.GetKeyUp(KeyCode.LeftArrow)) {
-                rb.velocity = new Vector2(0, rb.velocity.y);
-                animator.Play("ShrekIdle");
-                sprite.flipX = false;
-            }
-            if (Input.GetKeyUp(KeyCode.RightArrow)) {
-                rb.velocity = new Vector2(0, rb.velocity.y);
-                animator.Play("ShrekIdle");
-                sprite.flipX = false;
-            }
-            if (Input.GetKeyDown(KeyCode.UpArrow) && isOnGround) {
-                rb.velocity += Vector2.up * jumpHeight;
-                animator.Play("ShrekJump");
-            } 
-            if (Input.GetKeyDown(KeyCode.Z)) {
-                animator.Play("ShrekPunch");
-            }
-            if (Input.GetKeyDown(KeyCode.X)) {
-                animator.Play("ShrekKick");
-            }
+    void Update() {
+        print(rb.velocity + " " + (rb.velocity - prevVel));
+        prevVel = rb.velocity;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            rb.velocity += new Vector2(-movSpeed, 0);
+            animator.SetBool("isWalking", true);
+            sprite.flipX = true;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            rb.velocity += new Vector2(movSpeed, 0);
+            animator.SetBool("isWalking", true);
+            sprite.flipX = false;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow)) {
+            rb.velocity = new Vector2(0, 0);
+            animator.Play("ShrekIdle");
+            sprite.flipX = false;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            //rb.velocity += Vector2.up*jumpHeight;
+            animator.Play("ShrekJump");
+        } 
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            animator.Play("ShrekPunch");
+        }
+        if (Input.GetKeyDown(KeyCode.X)) {
+            animator.Play("ShrekKick");
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
+        print("CollisionEnter");
         if (collision.gameObject.tag.Equals(ground)) {
             isOnGround = true;
         }
     }
 
     void OnCollisionExit2D(Collision2D collision) {
+        print("CollisionExit");
         if (collision.gameObject.name.Equals(ground)) {
             isOnGround = false;
         }
