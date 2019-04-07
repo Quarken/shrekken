9ea75a59a9fecf7a690a027919430e53c1e8eb3f
Anders <au588409@uni.au.dk>
2019-04-07 11:38:01 +0200

    choose arena thingy

--- Assets/ChooseArena.cs
+++ Assets/ChooseArena.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChooseArena : MonoBehaviour{
     
@@ -9,42 +10,75 @@ public class ChooseArena : MonoBehaviour{
     public GameObject arena3;
     public GameObject arena4;
 
+    private Button arena1Button;
+    private Button arena2Button;
+    private Button arena3Button;
+    private Button arena4Button;
+
+    private Color normalColor;
+    private Color lightColor;
+
     
     // Start is called before the first frame update
-    void Start()
-    {
-        arena1.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+    void Start(){
+        
+        arena1Button = GameObject.Find("Arena1_button").GetComponent<UnityEngine.UI.Button>();
+        arena2Button = GameObject.Find("Arena2_button").GetComponent<UnityEngine.UI.Button>();
+        arena3Button = GameObject.Find("Arena3_button").GetComponent<UnityEngine.UI.Button>();
+        arena4Button = GameObject.Find("Arena4_button").GetComponent<UnityEngine.UI.Button>();
+
+        normalColor = new Color(109.0f, 60.0f, 11.0f);
+        lightColor = new Color(214.0f, 131.0f, 47.0f);
+
+        selectArena1();
     }
 
     public void selectArena1() {
-        arena1.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
-        arena2.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        arena3.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        arena4.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        normalSizeForAll();
+        arena1.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+        
+
         PlayerInfo.background = "Swamp";
     }
     
     public void selectArena2() {
-        arena1.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        arena2.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
-        arena3.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        arena4.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        normalSizeForAll();
+        arena2.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+        
+
         PlayerInfo.background = "Field";
     }
 
     public void selectArena3() {
-        arena1.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        arena2.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        arena3.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
-        arena4.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        normalSizeForAll();
+        arena3.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+
+
         PlayerInfo.background = "Boxingring";
     }
 
     public void selectArena4() {
+        normalSizeForAll();
+        arena4.transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+
+        PlayerInfo.background = "WhiteHouse";
+    }
+
+    public void normalSizeForAll() {
         arena1.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         arena2.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         arena3.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        arena4.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
-        PlayerInfo.background = "WhiteHouse";
+        arena4.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f); 
     }
+
+    public void normalColorForAll() {
+        arena1Button.GetComponent<Image>().color = normalColor;
+        arena2Button.GetComponent<Image>().color = normalColor;
+        arena3Button.GetComponent<Image>().color = normalColor;
+        arena4Button.GetComponent<Image>().color = normalColor;
+    }
+
+
+    
+
 }
